d2e7d9c220623f66a99f763395313fe40a22878f
mig-l-f <miguelandre.fernandes@gmail.com>
2024-09-08 13:20:18 +0100

    converter multi-ano pvgis para series autoproducao

--- src/aosol/series/producao.py
+++ src/aosol/series/producao.py
@@ -1,6 +1,7 @@
 """ Contem metodos para processar dados de producao
 """
 import pandas as pd
+import calendar
 
 def converter_pvgis_data(pvgis_tuple, ano):
     """ Converte um tuplo do pvgis para uma dataframe de autoproduco. 
@@ -30,4 +31,62 @@ def converter_pvgis_data(pvgis_tuple, ano):
     offset_minutos = df.index[-1].minute
     df.index = df.index - pd.DateOffset(minutes=offset_minutos)
 
+    return df
+
+def converter_pvgis_multiyear_ts(pvgis_tuple, ano):
+    """ Converter um tuplo pvgis com varios anos de dados calculando a potência média (P50) e P90
+    Assume que serie temporal tem valores horários. Isola os sinais de potência, converte para kW e translada para o ano pedido.
+
+    Fonte: https://solargis.com/resources/blog/best-practices/how-to-calculate-p90-or-other-pxx-pv-energy-yield-estimates
+    
+    Args:
+    -----
+    pvgis_tuple: tuple
+        Tuple com mais de 1 ano de dados obtido dos métodos pvgis
+    ano: int
+        Ano para qual queremos os timestamps finais.
+
+    Returns:
+    --------
+    df: pandas.DataFrame
+        Dataframe com colunas:
+          'autoproducao' em kW com média (P50) em cada registo
+          'autoproducao_p90' em kW com P90 em cada registo
+    """
+    # obter coluna de interesse, converter para kw
+    df = pvgis_tuple[0]['P'].to_frame('autoproducao') / 1000.0
+
+    # retirar offset minutos e por inicio hora
+    offset_minutos = df.index[-1].minute
+    df.index = df.index - pd.DateOffset(minutes=offset_minutos)
+
+    # groupby mes-dia hora
+    df = df.groupby('{:%m-%d %H}'.format).agg(['mean', 'std'])
+    # calcular P90 = P50 * (1 - P90 incerteza)
+    #  P90 incerteza = 1.282 * std
+    df[('autoproducao','p90')] = df[('autoproducao','mean')] * (1.0 - 1.282*df[('autoproducao','std')])
+
+    # renomear colunas
+    df.columns = list(map('_'.join, df.columns.values))
+    df.rename(columns={'autoproducao_mean':'autoproducao'}, inplace=True)
+    
+    # drop std
+    df.drop(['autoproducao_std'], axis=1, inplace=True)
+
+    # verificar se existe 29 Fev
+    df['mes'] = df.index.str[0:2]
+    df['dia'] = df.index.str[3:5]
+    df['hora'] = df.index.str[6:8]
+    has_29fev = ('02' in df['mes'].values) and ('29' in df['dia'].values)
+    
+    if has_29fev and (not calendar.isleap(ano)):
+        # remove dia 29 se ano alvo não é bisexto
+        df['a_remover'] = df['mes'].isin(['02']) & df['dia'].isin(['29'])
+        df = df[df['a_remover'] == False]
+        df.drop(['a_remover'], axis=1, inplace=True)
+
+    # converte index para formato %Y-%m-%d %H:%M
+    df.index = pd.to_datetime(str(ano) + '-' + df['mes'] + '-' + df['dia'] + ' ' + df['hora'] + ':00', format='%Y-%m-%d %H:%M')
+
+    df.drop(['mes', 'dia', 'hora'], axis=1, inplace=True)
     return df
--- src/testes/producao_test.py
+++ src/testes/producao_test.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import unittest
-from ..aosol.series import producao
+from ..aosol.series import producao, pvgis
 import pandas as pd
 
 class TestProducao(unittest.TestCase):
@@ -23,3 +23,82 @@ class TestProducao(unittest.TestCase):
         # potencia
         self.assertAlmostEqual(1.0, prod['autoproducao'].iloc[0], 2)
         self.assertAlmostEqual(2.0, prod['autoproducao'].iloc[-1], 2)
+
+    def test_converter_multiyear_ts(self):
+        # Given
+        df = pd.DataFrame({
+            'time' : ['2018-01-01 00:10', '2018-12-31 23:10', '2019-01-01 00:10', '2019-12-31 23:10'],
+            'P': [500.0, 1000.0, 700.0, 1200.0],
+            'poa_global': [10.0, 10.0, 10.0, 10.0]
+        })
+        df['time'] = pd.to_datetime(df['time'])
+        df = df.set_index('time')
+        dummy1 = []
+        dummy2 = []
+        pvgis_tuple = (df, dummy1, dummy2)
+
+        # When
+        prod = producao.converter_pvgis_multiyear_ts(pvgis_tuple, 2021)
+
+        # Then
+        self.assertEqual(2, len(prod))
+        self.assertEqual(datetime(2021, 1, 1, 0, 0, 0), prod.index[0])
+        self.assertEqual(datetime(2021, 12, 31, 23, 0, 0), prod.index[-1])
+        # media (P50)
+        self.assertAlmostEqual(0.6, prod['autoproducao'].iloc[0], 2)
+        self.assertAlmostEqual(1.1, prod['autoproducao'].iloc[-1], 2)
+        # P90
+        self.assertAlmostEqual(0.491, prod['autoproducao_p90'].iloc[0], 2)
+        self.assertAlmostEqual(0.900, prod['autoproducao_p90'].iloc[-1], 2)
+
+    def test_inputbisexto_multiano_alvo_naobisexto_pvgis(self):
+        # df contem 29/02 mas ano alvo nao é bisexto
+        df = pd.DataFrame({
+            'time' : ['2016-01-01 00:10', '2016-02-29 23:10', '2016-12-31 23:10', '2017-01-01 00:10', '2017-12-31 23:10'],
+            'P': [500.0, 2000.0, 1000.0, 700.0, 1200.0],
+            'poa_global': [10.0, 10.0, 10.0, 10.0, 10.0]
+        })
+        df['time'] = pd.to_datetime(df['time'])
+        df = df.set_index('time')
+        dummy1 = []
+        dummy2 = []
+        pvgis_tuple = (df, dummy1, dummy2)
+        ano_alvo = 2021
+
+        # When
+        prod = producao.converter_pvgis_multiyear_ts(pvgis_tuple, ano_alvo)
+
+        # Then
+        self.assertEqual(2, len(prod))
+        self.assertEqual(datetime(ano_alvo, 1, 1, 0, 0, 0), prod.index[0])
+        self.assertEqual(datetime(ano_alvo, 12, 31, 23, 0, 0), prod.index[-1])
+        # media
+        self.assertAlmostEqual(0.6, prod['autoproducao'].iloc[0], 2)
+        self.assertAlmostEqual(1.1, prod['autoproducao'].iloc[-1], 2)
+
+    def test_inputbisexto_multiano_alvo_bisexto_pvgis(self):
+        # df contem 29/02 mas ano alvo nao é bisexto
+        df = pd.DataFrame({
+            'time' : ['2016-01-01 00:10', '2016-02-29 23:10', '2016-12-31 23:10', '2017-01-01 00:10', '2017-12-31 23:10'],
+            'P': [500.0, 2000.0, 1000.0, 700.0, 1200.0],
+            'poa_global': [10.0, 10.0, 10.0, 10.0, 10.0]
+        })
+        df['time'] = pd.to_datetime(df['time'])
+        df = df.set_index('time')
+        dummy1 = []
+        dummy2 = []
+        pvgis_tuple = (df, dummy1, dummy2)
+        ano_alvo = 2020
+
+        # When
+        prod = producao.converter_pvgis_multiyear_ts(pvgis_tuple, ano_alvo)
+
+        # Then
+        self.assertEqual(3, len(prod))
+        self.assertEqual(datetime(ano_alvo, 1, 1, 0, 0, 0), prod.index[0])
+        self.assertEqual(datetime(ano_alvo, 2, 29, 23, 0, 0), prod.index[1])
+        self.assertEqual(datetime(ano_alvo, 12, 31, 23, 0, 0), prod.index[-1])
+        # media
+        self.assertAlmostEqual(0.6, prod['autoproducao'].iloc[0], 2)
+        self.assertAlmostEqual(2.0, prod['autoproducao'].iloc[1], 2)
+        self.assertAlmostEqual(1.1, prod['autoproducao'].iloc[-1], 2)
